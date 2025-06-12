9e287c7c94082ebb6e892e97eec1057907d933ca
najnomics <jesuorobonosakhare873@gmail.com>
2025-06-12 19:26:00 +0100

    h

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -1,19 +1,23 @@
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+javascript
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// sgMail.setDataResidency('eu'); 
+// uncomment the above line if you are sending mail using a regional EU subuser
 
 const msg = {
   to: 'test@example.com', // Change to your recipient
-  from: 'team@elitegloblinternships.com', // Use the email you verified in SendGrid
-  subject: 'Sending with Twilio SendGrid is Fun',
+  from: 'test@example.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-
+}
 sgMail
   .send(msg)
   .then(() => {
-    console.log('Email sent');
+    console.log('Email sent')
   })
   .catch((error) => {
-    console.error(error);
-  });
+    console.error(error)
+  })
